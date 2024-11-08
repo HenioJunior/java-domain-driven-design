fb8c76cc4d841ebdc3fff78743ade600a720fcf7
Henio Junior <henioajunior@gmail.com>
2024-11-08 07:02:19 -0300

    Separado as camadas por responsabilidade

--- src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -1,7 +1,6 @@
 package br.com.alura.codechella.dominio.evento;
 
-import br.com.alura.codechella.Categoria;
-import br.com.alura.codechella.dominio.tipo.TipoIngresso;
+import br.com.alura.codechella.dominio.ingresso.TipoIngresso;
 
 import java.time.LocalDateTime;
 import java.util.ArrayList;
---
+++ src/main/java/br/com/alura/codechella/dominio/ingresso/TipoIngresso.java
@@ -0,0 +1,9 @@
+package br.com.alura.codechella.dominio.ingresso;
+
+public class TipoIngresso {
+    private Integer codigo;
+    private Setor setor;
+    private Definicao definicao;
+    private Double valor;
+    private int totalDisponivel;
+}
