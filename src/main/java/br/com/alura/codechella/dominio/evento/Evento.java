86368107805b47630eb0c607474dd1d134bb7726
Henio Junior <henioajunior@gmail.com>
2024-11-08 11:20:31 -0300

    Criado a fábrica de eventos

--- src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -15,13 +15,40 @@ public class Evento {
     private LocalDateTime data;
     private List<TipoIngresso> tipoIngressos = new ArrayList<>();
 
-    public Evento(Categoria categoria, String descricao, Endereco endereco, LocalDateTime data) {
-        this.categoria = categoria;
-        this.descricao = descricao;
-        this.endereco = endereco;
-        this.data = data;
+    private Evento() {}
 
-        gerarIdentificadorUnico();
+    public static class Builder {
+        private Evento evento;
+
+        public Builder() {
+            evento = new Evento();
+        }
+
+        public Builder comCategoria(Categoria categoria) {
+            evento.categoria = categoria;
+            return this;
+        }
+
+        public Builder comDescricao(String descricao) {
+            evento.descricao = descricao;
+            return this;
+        }
+
+        public Builder comEndereco(String cep, Integer numero, String complemento) {
+            Endereco endereco = new Endereco(cep, numero, complemento);
+            evento.endereco = endereco;
+            return this;
+        }
+
+        public Builder comData(LocalDateTime data) {
+            evento.data = data;
+            return this;
+        }
+
+        public Evento build() {
+            evento.gerarIdentificadorUnico();
+            return evento;
+        }
     }
 
     private void gerarIdentificadorUnico() {
@@ -34,5 +61,15 @@ public class Evento {
 
     public void aumentarNumeroDeIngressosPorTipo() {
         //Implementar
+    }
+
+    @Override
+    public String toString() {
+        return "Evento{" +
+                "categoria=" + categoria +
+                ", descricao='" + descricao + '\'' +
+                ", endereco=" + endereco +
+                ", data=" + data +
+                '}';
     }
 }
