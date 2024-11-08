7295d387d712b7c2d0b5230ab0682ee1e48ddec9
Henio Junior <henioajunior@gmail.com>
2024-11-07 21:24:05 -0300

    Alterado nome da classe para Setor, Criado classes de dominio

---
+++ src/main/java/br/com/alura/codechella/dominio/evento/Evento.java
@@ -0,0 +1,24 @@
+package br.com.alura.codechella.dominio.evento;
+
+import br.com.alura.codechella.Categoria;
+import br.com.alura.codechella.dominio.tipo.TipoIngresso;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Evento {
+    private Categoria categoria;
+    private String descricao;
+    private Endereco endereco;
+    private LocalDateTime data;
+    private List<TipoIngresso> tipoIngressos = new ArrayList<>();
+
+    public void incluiNovoTipoDeIngressoAoEvento(TipoIngresso tipoIngresso) {
+        this.tipoIngressos.add(tipoIngresso);
+    }
+
+    public void aumentarNumeroDeIngressosPorTipo() {
+        //Implementar
+    }
+}
---
+++ src/main/java/br/com/alura/codechella/dominio/tipo/TipoIngresso.java
@@ -0,0 +1,12 @@
+package br.com.alura.codechella.dominio.tipo;
+
+import br.com.alura.codechella.Definicao;
+import br.com.alura.codechella.Setor;
+
+public class TipoIngresso {
+    private Integer codigo;
+    private Setor setor;
+    private Definicao definicao;
+    private Double valor;
+    private int totalDisponivel;
+}
